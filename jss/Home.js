2eb6f9ba36c9e09645854bac1b472fed1cfae070
Anthomy3000 <114633797+Anthomy3000@users.noreply.github.com>
2024-03-08 15:28:22 +0800

    Update Home.js

--- jss/Home.js
+++ jss/Home.js
@@ -9,7 +9,7 @@ const navItems = [nav1, nav2, nav3, nav4, nav5];
 //Changing to index animation
 var timer = setTimeout(function() {
             window.location='https://anthomy3000.github.io/anthomytan.github.io/index.html'
-        }, 50000);
+        }, 100000);
 
 // Control Navigation Animation
 function navAnimation(val1, val2) {
